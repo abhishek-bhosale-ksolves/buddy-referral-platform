e6ea0bfc927d05aa8ee1f98ac13fc1f0c8a45990
AbhishekASLK <abhishek.bhosale@ksolves.com>
2025-03-20 15:18:34 +0530

    make use of / for swagger api

--- server.js
+++ server.js
@@ -18,13 +18,9 @@ app.use((err, req, res, next) => {
 // Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/referrals', require('./routes/referrals'));
-app.use('/',(req, res) => {
-    res.send('Welcome to the Referral API');
-  }
-);
 
 // Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(process.env.PORT, function() {
   console.log('Server is running on port 3000');
